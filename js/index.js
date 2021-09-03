617facc6a0f4d45030463da56803ecd227803d36
didebbo <gianluca.napoletano.93@gmail.com>
2021-09-03 08:27:08 +0200

    init 2.0

--- js/index.js
+++ js/index.js
@@ -2,49 +2,66 @@
 const dom_Game = document.getElementById("game");
 
 // Parametri
-const bombs = 16;
+const bombs = 1;
 let score = 0;
 let colSize = 10;
 let gridSize = colSize * colSize;
 let gameOver = false;
 let cellClicked = [];
 let cellBombs = [];
+let matrix = [];
 
 // Functions
 
-const checkBombsAround = (id) => {
+const checkBombsAround = (dom_Td) => {
     let counter = 0;
-    let log = "Cell: " + id;
+    let rowIndex = dom_Td.parentNode.rowIndex;
+    let cellIndex = dom_Td.cellIndex;
+
+    alert("[Cell] rowIndex: " + (rowIndex + 1) + " cellIndex: " + (cellIndex + 1));
+
+    // console.log(parseInt(dom_Td.dataset.bomb));
+
+    let log = "[Target] rowIndex: " + (rowIndex + 1) + " cellIndex: " + (cellIndex + 1);
+
     // Up
-    log += "\nUp: " + (id - colSize);
-    if (cellBombs.includes(id - colSize)) counter++;
+    // TODO Controlli celle angolari
+    log += "\n[Up] rowIndex: " + (rowIndex - 1) + " cellIndex: " + (cellIndex);
+    if (parseInt(document.getElementsByTagName("table")[0].children[rowIndex - 1].children[cellIndex].dataset.bomb)) counter++;
+
     // UpLeft
-    log += "\nUpLeft: " + (id - colSize - 1);
-    if (cellBombs.includes(id - colSize - 1)) counter++;
-    // UpRight
-    log += "\nUpRight: " + (id - colSize + 1);
-    if (cellBombs.includes(id - colSize + 1)) counter++;
+    log += "\n[UpLeft] rowIndex: " + (rowIndex - 1) + " cellIndex: " + (cellIndex - 1);
+    if (parseInt(document.getElementsByTagName("table")[0].children[rowIndex - 1].children[cellIndex - 1].dataset.bomb)) counter++;
+
+    //UpRight
+    log += "\n[UpRight] rowIndex: " + (rowIndex - 1) + " cellIndex: " + (cellIndex + 1);
+    if (parseInt(document.getElementsByTagName("table")[0].children[rowIndex - 1].children[cellIndex + 1].dataset.bomb)) counter++;
+
     // Down
-    log += "\nDown: " + (id + colSize);
-    if (cellBombs.includes(id + colSize)) counter++;
+    log += "\n[Down] rowIndex: " + (rowIndex + 1) + " cellIndex: " + (cellIndex);
+    if (parseInt(document.getElementsByTagName("table")[0].children[rowIndex + 1].children[cellIndex].dataset.bomb)) counter++;
+
     // DownLeft
-    log += "\nDownLeft: " + (id + colSize - 1);
-    if (cellBombs.includes(id + colSize - 1)) counter++;
+    log += "\n[DownLeft] rowIndex: " + (rowIndex + 1) + " cellIndex: " + (cellIndex - 1);
+    if (parseInt(document.getElementsByTagName("table")[0].children[rowIndex + 1].children[cellIndex - 1].dataset.bomb)) counter++;
+
     // DownRight
-    log += "\nDownRight: " + (id + colSize + 1);
-    if (cellBombs.includes(id + colSize + 1)) counter++;
+    log += "\n[DownRight] rowIndex: " + (rowIndex + 1) + " cellIndex: " + (cellIndex + 1);
+    if (parseInt(document.getElementsByTagName("table")[0].children[rowIndex + 1].children[cellIndex + 1].dataset.bomb)) counter++;
+
     // Left
-    log += "\nLeft: " + (id - 1);
-    if (cellBombs.includes(id - 1)) counter++;
+    log += "\n[Left] rowIndex: " + (rowIndex) + " cellIndex: " + (cellIndex - 1);
+    if (parseInt(document.getElementsByTagName("table")[0].children[rowIndex].children[cellIndex - 1].dataset.bomb)) counter++;
+
     // Right
-    log += "\nRight: " + (id + 1);
-    if (cellBombs.includes(id + 1)) counter++;
+    log += "\n[Right] rowIndex: " + (rowIndex) + " cellIndex: " + (cellIndex + 1);
+    if (parseInt(document.getElementsByTagName("table")[0].children[rowIndex].children[cellIndex + 1].dataset.bomb)) counter++;
 
     log += "\nBombs around: " + counter;
 
-    // document.getElementById(id).innerHTML = counter;
+    dom_Td.innerHTML = counter;
 
-    // alert(log);
+    alert(log);
 
 }
 
@@ -54,9 +71,21 @@ const discoverBombs = () => {
     }
 }
 
-const clickEvent = (dom_Div) => {
-    dom_Div.addEventListener("click", (e) => {
-        let id = parseInt(e.target.getAttribute("id"));
+const clickEvent = (dom_Td) => {
+    dom_Td.addEventListener("click", () => {
+
+        console.log("Bomb: " + parseInt(dom_Td.dataset.bomb));
+
+        if (!parseInt(dom_Td.dataset.bomb)) {
+            dom_Td.classList.add("good");
+            score++;
+            checkBombsAround(dom_Td);
+        }
+        else {
+            dom_Td.classList.add("boom");
+        }
+        return;
+        let id = parseInt(e.target);
         let countBombs = 0;
         if (gameOver) return;
         if (!cellClicked.includes(id)) {
@@ -72,6 +101,7 @@ const clickEvent = (dom_Div) => {
                     discoverBombs();
                     setTimeout(() => {
                         alert(log);
+                        location.reload();
                     }, 100);
                 }
 
@@ -83,38 +113,61 @@ const clickEvent = (dom_Div) => {
                 log += "\nPunteggio: " + score;
                 setTimeout(() => {
                     alert(log);
+                    location.reload();
                 }, 100);
             }
         }
     })
 }
 
-const drawGrid = (colSize) => {
-    gridSize = colSize * colSize;
-    let dom_Grid = document.createElement("ul");
-    dom_Game.appendChild(dom_Grid);
-    for (let i = 1; i <= gridSize; i++) {
-        let dom_Cell = document.createElement("li");
-        let dom_Div = document.createElement("div");
-        dom_Div.setAttribute("id", i);
-        // dom_Div.innerHTML = i;
-        dom_Cell.appendChild(dom_Div);
-        dom_Grid.appendChild(dom_Cell);
-
-        // Style 
-        dom_Cell.style.width = "calc(100% / " + colSize + ")";
-
-        // Click Event
-        clickEvent(dom_Div);
+const genMatrix = () => {
+    // matrix.length = colSize;
+    for (let x = 0; x < colSize; x++) {
+        matrix[x] = [];
+        for (let y = 0; y < colSize; y++) {
+            matrix[x][y] = y;
+            for (let z = 0; z < x; z++) {
+                // TODO MATRIX
+            }
+        }
     }
+    console.log(matrix);
 }
 
-const setBombs = () => {
+const genGrid = () => {
+    let dom_Table = document.createElement("table");
+    let dom_Tr;
+    let dom_Td;
+    let counter = 1;
+    dom_Game.appendChild(dom_Table);
+    for (let y = 0; y < colSize; y++) {
+        dom_Tr = document.createElement("tr");
+        for (let x = 0; x < colSize; x++) {
+            dom_Td = document.createElement("td");
+            if (cellBombs.includes(counter)) dom_Td.dataset.bomb = 1;
+            else dom_Td.dataset.bomb = 0;
+            clickEvent(dom_Td);
+            dom_Tr.appendChild(dom_Td);
+            counter++;
+        }
+        dom_Table.appendChild(dom_Tr);
+
+        // Style 
+        dom_Td.style.width = "calc(100% / " + colSize + ")";
+        dom_Td.style.height = "calc(100% / " + colSize + ")";
+    }
+
+    // console.log(dom_Table.children[1].children);
+}
+
+const genBombs = () => {
+    gridSize = colSize * colSize;
     let currentBomb = 1;
     while (currentBomb <= bombs) {
-        let positionBomb = Math.floor(Math.random() * gridSize) + 1;
+        let positionBomb = 5 /*Math.floor(Math.random() * gridSize) + 1*/;
         if (!cellBombs.includes(positionBomb)) {
             cellBombs.push(positionBomb);
+            cellBombs.sort(function (a, b) { return a - b });
             currentBomb++;
         }
     }
@@ -125,8 +178,9 @@ const createGrid = (level) => {
     if (level > 1) colSize -= 2;
     if (level > 2) colSize -= 3;
 
-    drawGrid(colSize);
-    setBombs();
+    // genMatrix();
+    genBombs();
+    genGrid();
 }
 
 const main = () => {
@@ -137,6 +191,7 @@ const main = () => {
     while (isNaN(level) || level < 1 || level > 3);
     createGrid(level);
 }
+
 
 // Main
 main();
